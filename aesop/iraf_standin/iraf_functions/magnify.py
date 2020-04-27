baf0292ea0a3da2d46bde286c3a8ebc849eabed8
MNBrod <max.brodheim@hws.edu>
2020-04-27 12:08:15 -0400

    implements flux conservation in magnify using IRAF method

--- aesop/iraf_standin/iraf_functions/magnify.py
+++ aesop/iraf_standin/iraf_functions/magnify.py
@@ -1,13 +1,18 @@
 from scipy import ndimage
 from astropy.io import fits
 from shutil import copyfile
+import numpy as np
+import matplotlib.pyplot as plt
 
 __all__ = ['magnify_y']
 
 def magnify_y(input_name, output_name, mag=4):
     '''
-    Magnifies an image in the y-direction by the given factor. Does NOT
-    conserve flux.
+    Magnifies an image in the y-direction by the given factor.
+
+    Implementes rough conservation of flux by scaling each image pizel
+    by the ratio out input/output image area (e.g. magnifying by 4 will
+    scale each pixel by .25)
 
     Parameters
     -------
@@ -23,10 +28,17 @@ def magnify_y(input_name, output_name, mag=4):
     hdul = fits.open(input_name)
     
     image = hdul[0].data
-    
-    hdul[0].data = ndimage.zoom(image, (mag, 1), order=1,
+
+    # Trim image based on known detector size (ignore overscan region)
+    image = image[0:2048,0:2048]
+
+    magnified = ndimage.zoom(image, (mag, 1), order=1,
         mode='nearest')
     
+    magnified = magnified // mag
+
+    hdul[0].data = magnified
+
     hdul.writeto(open(output_name, "wb"))
     
     hdul.close()
